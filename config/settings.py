4393f58795d6c2dff1e124997ebdd0c7e729ccde
kimkiyun <kiyoon9390@gmail.com>
2020-09-12 17:05:18 +0900

    dev_branch first commit

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+PICKY_APPS = [
+
+]
+
+INSTALLED_APPS = DJANGO_APPS + PICKY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
