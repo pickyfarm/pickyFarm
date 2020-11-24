f3a7c217506c0df447cf0ace12670506a3ac1515
SJKim <hedro0458@gmail.com>
2020-11-24 21:39:21 +0900

    editors pick feature updated

--- editor_reviews/views.py
+++ editor_reviews/views.py
@@ -11,6 +11,7 @@ def index(request):
 
     return render(request, 'editors_pick/index.html', ctx)
 
+
 def detail(request, pk):
     review = get_object_or_404(Editor_Reviews, pk=pk)
     ctx = {
@@ -19,3 +20,38 @@ def detail(request, pk):
     return render(request, 'editors_pick/details.html', ctx)
 # Create your views here.
 
+
+def create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+
+        else:
+            form = PostForm()
+        return render(request, 'editors_pick/create.html', {'form': form})
+        
+
+def update(request, pk):
+    post = get_object_or_404(Editor_Reviews, pk=pk)
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('detail', pk)
+
+    else:
+        form = PostForm(instance=post)
+    
+    return render(request, 'editors_pick/update.html', {'forms': form})
+    
+
+def delete(request, pk):
+
+    post = Editor_Reviews.object.get(pk=pk)
+    post.delete()
+    
+    return redirect('index')
