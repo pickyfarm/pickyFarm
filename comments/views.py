929e2ca558429cc1c1df450755943cc4373f6b77
InjiChoi <chldlswl1212@gmail.com>
2021-03-28 02:24:05 +0900

    [fix] product 리뷰 댓글 ajax (todo: ajax view 수정)

--- comments/views.py
+++ comments/views.py
@@ -3,7 +3,8 @@ from django.urls import reverse
 from .models import Product_Comment, Product_Recomment
 from .forms import ProductCommentForm, ProductRecommentForm
 from products.models import Product
-# from django.http import JsonResponse
+from django.template.loader import render_to_string
+from django.http import JsonResponse
 
 # 상품 댓글 Read
 def product_comment_detail(request, pk):
@@ -77,12 +78,12 @@ def product_recomment_detail(request, pk):
 
     return render(request, 'comments/product_comment.html', ctx)
 
-# 상품 대댓글(리뷰 댓글) create ajax
+# 상품 대댓글 create
 def product_recomment_create(request, pk):
+    is_ajax = request.POST.get('is_ajax')
+    print(is_ajax)
     product_comment = Product_Comment.objects.get(pk=pk)
-    print(product_comment.text)
     product = product_comment.product
-    print(product)
     recomment_form = ProductRecommentForm(request.POST)
     if recomment_form.is_valid():
         print('valid')
@@ -90,6 +91,13 @@ def product_recomment_create(request, pk):
         recomment.author = request.user
         recomment.comment = product_comment
         recomment.save()
+
+    if is_ajax:
+        print("ajax성공")
+        html = render_to_string('products/product_detail.html',{'recomment':recomment, 'username':request.user})
+        return JsonResponse({'html':html})
+    else:
+        print("ajax실패")
 
     return redirect('products:product_detail', product.pk)
     
