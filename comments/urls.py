8e39a3af2d67570617686063f8c3cbb3c677412b
InjiChoi <chldlswl1212@gmail.com>
2021-03-31 19:31:04 +0900

    [FIX] 상품 리뷰 댓글(product recomment) 작성 ajax 완료(todo: 스크롤 최하단 고정)

--- comments/urls.py
+++ comments/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     # product
     path('product_comment/<int:pk>/', views.product_comment_detail, name="product_comment_detail"),
     path('product_comment/<int:pk>/recomment/', views.product_recomment_detail, name="product_recomment_detail"),
-    path('product_comment/<int:pk>/recomment/create', views.product_recomment_create, name="product_recomment_create"),
+    path('<int:comment_id>/recomment/create/', views.product_recomment_create, name="product_recomment_create"),
     
     # editors_review
     # path('editors_review_comment/<int:pk>/', views.editors_review_comment, name="editors_review_comment"),
