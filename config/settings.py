b5c434265a56f044f8ed1022d6bc85631761d8c3
kimkiyun <kiyoon9390@gmail.com>
2021-04-27 17:11:35 +0900

    [FIX] static 폴더 명 변경

--- config/settings.py
+++ config/settings.py
@@ -164,9 +164,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static_devs'),)
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
