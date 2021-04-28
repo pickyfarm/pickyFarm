fb992f6d335b42a9f989858a99adfb71c6770a23
SJKim <hedro0458@gmail.com>
2021-04-28 23:25:15 +0900

    fix : seed_farmer 수정

--- core/management/commands/seed_farmer.py
+++ core/management/commands/seed_farmer.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from users import models as user_models
 from farmers import models as farmer_models
+from addresses import models as address_models
 from faker import Faker
 import random
 
@@ -8,11 +9,12 @@ import random
 class Command(BaseCommand):
     def handle(self, *args, **options):
 
-        users = user_models.User.objects.order_by('?')[:5]
+        users = user_models.User.objects.order_by("?")[:5]
+        addresses = address_models.Address.objects.order_by("?")[:5]
         faker = Faker("ko-KR")
-        for user in users:
+        for i in range(5):
             farmer_models.Farmer.objects.create(
-                user=user,
+                user=users[i],
                 farm_name=faker.bs(),
                 farm_profile=faker.file_name(),
                 farmer_profile=faker.file_name(),
@@ -20,5 +22,6 @@ class Command(BaseCommand):
                 profile_desc=faker.paragraph(),
                 sub_count=random.randint(100, 1000),
                 farm_news=faker.sentence(nb_words=5),
-                farm_cat=random.choice(farmer_models.Farmer.CAT_CHOICES)
+                farm_cat=random.choice(farmer_models.Farmer.CAT_CHOICES),
+                address=addresses[i],
             )
