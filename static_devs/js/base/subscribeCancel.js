e9d3c853fe957fffbba7d84e6ef3abe2b2fbc438
kim_ki_yun <kiyoon9390@gmail.com>
2021-06-21 18:38:34 +0900

    [CREATE] 장바구니 담기 ajax - js 파일로 분리

--- static_devs/js/base/subscribeCancel.js
+++ static_devs/js/base/subscribeCancel.js
@@ -1,32 +1,4 @@
-function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = cookies[i].trim();
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
 
-let csrftoken = getCookie('csrftoken');
-
-function csrfSafeMethod(method) {
-    // these HTTP methods do not require CSRF protection
-    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-}
-$.ajaxSetup({
-    beforeSend: function (xhr, settings) {
-        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-            xhr.setRequestHeader("X-CSRFToken", csrftoken);
-        }
-    }
-});
 
 const subscribeCancel = (pk)=>{
     let data = { 'pk': pk };
@@ -37,7 +9,7 @@ const subscribeCancel = (pk)=>{
         data: data,
         success: function (response) {
             if (response.success == 0) {
-                shootToastMessage("다시 시도해주세요", 2.5, alert);
+                shootToastMessage("다시 시도해주세요", 2.5, error);
             }
             else {
                 console.log(response.success)
