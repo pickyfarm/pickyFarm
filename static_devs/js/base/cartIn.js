ca48d39c0e7006dde79eba880cd5ceac6b278339
SJKim <hedro0458@gmail.com>
2021-06-24 17:47:24 +0900

    feat: add cart-in feature when click cart buttons

--- static_devs/js/base/cartIn.js
+++ static_devs/js/base/cartIn.js
@@ -1,18 +1,17 @@
-
-
-const cartIn = (pk) =>{
+const cartIn = (pk) => {
     $.ajax({
-        type: "POST",
+        type: 'POST',
         url: '/user/cartIn/',
         data: {
-            "pk": pk,
+            pk: pk,
+            csrfmiddlewaretoken: csrftoken,
         },
-        dataType: "json",
+        dataType: 'json',
         success: function (response) {
             shootToastMessage(response.message, 2);
         },
         error: function (request, status, error) {
-            shootToastMessage("로그인이 필요합니다.", 2, error)
-        }
+            shootToastMessage('로그인이 필요합니다.', 2, 'error');
+        },
     });
-}
+};
