4376db4bf4d8346e8bb2b5e9a87b8d721e57cb05
kim_ki_yun <kiyoon9390@gmail.com>
2021-06-26 21:03:20 +0900

    [CREATE] import base64

--- orders/views.py
+++ orders/views.py
@@ -171,7 +171,7 @@ def payment_success(request):
                     'amount':amount_paid,
                 }
                 # PG사에서 제공해주는 client ID and Client Passwords
-                usr_pass = b"test_ck_MGjLJoQ1aVZREgzG4ogVw6KYe2RN:test_sk_BE92LAa5PVb64R41qaPV7YmpXyJj"
+                usr_pass = b"test_sk_BE92LAa5PVb64R41qaPV7YmpXyJj:"
                 # b64로 암호화
                 b64_val = base64.b64encode(usr_pass).decode('utf-8')
 
