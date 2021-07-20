3020e121a375f4ebf526b23828888391f07096d8
InjiChoi <chldlswl1212@gmail.com>
2021-07-20 19:38:11 +0900

    [FIX] storage python file error fix

--- kakaomessages/storage.py
+++ kakaomessages/storage.py
@@ -9,7 +9,7 @@ def uploadImage(path):
     with open(path, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
     data = {"file": str(encoded_string)[2:-1], "type": "MMS"}
-    headers = auth.get_headers(settings.apiKey, settings.apiSecret)
+    headers = get_headers(settings.apiKey, settings.apiSecret)
     return requests.post(settings.getUrl("/storage/v1/files"), headers=headers, json=data)
 
 
@@ -17,5 +17,5 @@ def uploadKakaoImage(path):
     with open(path, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
     data = {"file": str(encoded_string)[2:-1], "type": "KAKAO"}
-    headers = auth.get_headers(settings.apiKey, settings.apiSecret)
+    headers = get_headers(settings.apiKey, settings.apiSecret)
     return requests.post(settings.getUrl("/storage/v1/files"), headers=headers, json=data)
