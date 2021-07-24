83b35e247dde5f960f5cad79cf09f48b945bb172
SJKim <hedro0458@gmail.com>
2021-07-25 02:42:40 +0900

    fix: fix payment valid

--- farmers/models.py
+++ farmers/models.py
@@ -11,17 +11,22 @@ class Farmer(models.Model):
     farmer_profile = models.ImageField(
         upload_to="farmer_profile/%Y/%m/%d/", null=True, blank=True
     )  # 농장주 사진 default icon 설정
-    farm_profile = models.ImageField(upload_to="farm_profile/%Y/%m/%d/")  # 농장 대표사진 or 로고
+    farm_profile = models.ImageField(
+        upload_to="farm_profile/%Y/%m/%d/"
+    )  # 농장 대표사진 or 로고
     profile_title = models.CharField(max_length=200)  # 농가 한 줄 소개
     farm_desc = models.ImageField(
         upload_to="farm_desc/%Y/%m/%d/", null=True, blank=True
     )  # 농가 상세 소개
     sub_count = models.IntegerField(default=0)  # 구독자 수
     farm_news = models.CharField(max_length=500, null=True, blank=True)  # 농가 뉴스
+    farm_thanks_msg = models.CharField(max_length=500)  # 농가 구매 감사 메세지
     farm_cat = models.CharField(choices=CAT_CHOICES, max_length=20, default="fruit")
     contract = models.BooleanField(default=False)  # 계약서 동의 여부
     open = models.BooleanField(default=False)  # 입점 승인 여부
-    user = models.OneToOneField("users.User", related_name="farmer", on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        "users.User", related_name="farmer", on_delete=models.CASCADE
+    )
     address = models.OneToOneField(
         "addresses.Address", related_name="farmer", on_delete=models.CASCADE
     )
