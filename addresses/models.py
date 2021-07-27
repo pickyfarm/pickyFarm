a550cda86d81391f4320b5d5fc5d810ac2c9ad83
SJKim <hedro0458@gmail.com>
2021-07-27 23:05:45 +0900

    feat: add get_full_address to addresses model

--- addresses/models.py
+++ addresses/models.py
@@ -10,11 +10,13 @@ class Address(models.Model):
     sigungu = models.CharField(max_length=30, blank=True)
     is_default = models.BooleanField(default=False)
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='addresses')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="addresses")
 
     update_at = models.DateTimeField(auto_now=True)
     create_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.user.nickname}'
-    
+        return f"{self.user.nickname}"
+
+    def get_full_address(self):
+        return f"{self.full_address} {self.detail_address if self.detail_address else ''} {self.extra_address if self.extra_address else ''}"
