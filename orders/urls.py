aece22069f03ab208f8a77e1ddd6168e5b3f54e8
JinHee <327jhl@naver.com>
2021-07-30 16:13:40 +0900

    feat: 주문/결제 완료 페이지 구현(html,css)
    template tag를 통한 주문 물품 리스트 출력 작업 및 동적 페이지 구현을 위한 Javascript 작업(농장 구독시 Ajax로 표현되게 한다던지)이 추가로 필요할 것 같음

--- orders/urls.py
+++ orders/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
    path('payment/', views.payment_create, name="payment_create"),
    path('payment/success', views.payment_success, name="payment_success"),
    path('payemnt/fail', views.payment_fail, name="payment_fail"),
-   path('payment/valid', views.payment_valid, name="payment_valid")
+   path('payment/valid', views.payment_valid, name="payment_valid"),
+
+   # 주문/결제 완료 페이지 작업하기 위한 임시 url
+   # path('temporary/payment/success', views.temporary_payment_success, name="temporary_payment_success")
 ]
