cb2093fe662f1fc06563dbd85b91621bfcba1e65
root <root@ip-172-31-51-165.ap-northeast-2.compute.internal>
2021-08-03 06:32:48 +0000

    commit

--- config/settings.py
+++ config/settings.py
@@ -252,6 +252,7 @@ AWS_S3_CUSTOM_DOMAIN = "%s.s3.%s.amazonaws.com" % (AWS_STORAGE_BUCKET_NAME, AWS_
 AWS_S3_OBJECT_PARAMETERS = {
     "CacheControl": "max-age=86400",
 }
+AWS_DEFAULT_ACL = os.environ.get("AWS_DEFAULT_ACL")
 
 # AWS_LOCATION = "static"
 # STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
