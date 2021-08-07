fd8a88bc72f18fea7d15f9a7b1c2fb361806fda8
SJKim <hedro0458@gmail.com>
2021-08-07 16:42:32 +0900

    feat: set order_cancel procedure atomic

--- orders/views.py
+++ orders/views.py
@@ -576,6 +576,7 @@ def payment_valid(request):
 
 
 @login_required
+@transaction.atomic
 def order_cancel(request, pk):
     # http referer 참고해서 임의 접근 막는 코드 넣을 예정
     order = Order_Detail.objects.get(pk=pk)
