18e4076606dede4799170421766e391cc625d5b6
InjiChoi <chldlswl1212@gmail.com>
2021-08-15 03:47:45 +0900

    feat: separate settings for deploy and debug

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import dotenv
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.debug")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
