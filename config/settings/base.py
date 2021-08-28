2ded6d9e81b44c9f332ace60265e3f0f423570a8
SJKim <hedro0458@gmail.com>
2021-08-28 21:21:59 +0900

    fix: apply comment

--- config/settings/base.py
+++ config/settings/base.py
@@ -167,12 +167,12 @@ TEMPLATES = [
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#     }
+# }
 
 
 # Password validation
