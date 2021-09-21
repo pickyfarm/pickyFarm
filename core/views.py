d6d0acc1c0e90dd3b3635d636941b39c5826293b
SJKim <hedro0458@gmail.com>
2021-09-21 20:00:49 +0900

    feat: add landing page base

--- core/views.py
+++ core/views.py
@@ -10,48 +10,48 @@ from django.views.generic import TemplateView
 
 def index(request):
 
-    try:
-        products = Product.objects.filter(open=True)
+    # try:
+    #     products = Product.objects.filter(open=True)
 
-    except ObjectDoesNotExist:
-        pass
+    # except ObjectDoesNotExist:
+    #     pass
 
-    if len(products) < 5:
-        today_pick_list = products.order_by("create_at")[: len(products)]
-        best_product_list = products.order_by("sales_rate")[: len(products)]
+    # if len(products) < 5:
+    #     today_pick_list = products.order_by("create_at")[: len(products)]
+    #     best_product_list = products.order_by("sales_rate")[: len(products)]
 
-    elif len(products) < 4:
-        today_pick_list = products.order_by("create_at")[: len(products)]
-        best_product_list = products.order_by("sales_rate")[: len(products)]
+    # elif len(products) < 4:
+    #     today_pick_list = products.order_by("create_at")[: len(products)]
+    #     best_product_list = products.order_by("sales_rate")[: len(products)]
 
-    else:
-        today_pick_list = products.order_by("create_at")[:5]
-        best_product_list = products.order_by("sales_rate")[0:4]
+    # else:
+    #     today_pick_list = products.order_by("create_at")[:5]
+    #     best_product_list = products.order_by("sales_rate")[0:4]
 
-    try:
-        editor_pick_list = Editor_Review.objects.all()
+    # try:
+    #     editor_pick_list = Editor_Review.objects.all()
 
-    except Editor_Review.DoesNotExist:
-        pass
+    # except Editor_Review.DoesNotExist:
+    #     pass
 
-    today_farmer_list = Farmer.objects.all()
-    main_slider_image = Main_Slider_Image.objects.all()
+    # today_farmer_list = Farmer.objects.all()
+    # main_slider_image = Main_Slider_Image.objects.all()
 
-    if len(today_farmer_list) < 3:
-        today_farmer_list = (
-            today_farmer_list | Farmer.objects.all()[: 3 - len(today_farmer_list)]
-        )
+    # if len(today_farmer_list) < 3:
+    #     today_farmer_list = (
+    #         today_farmer_list | Farmer.objects.all()[: 3 - len(today_farmer_list)]
+    #     )
 
-    ctx = {
-        "products": products,
-        "today_pick_list": today_pick_list,
-        "best_product_list": best_product_list,
-        "editor_pick_list": editor_pick_list,
-        "today_farmer_list": today_farmer_list,
-        "main_slider_image": main_slider_image,
-    }
+    # ctx = {
+    #     "products": products,
+    #     "today_pick_list": today_pick_list,
+    #     "best_product_list": best_product_list,
+    #     "editor_pick_list": editor_pick_list,
+    #     "today_farmer_list": today_farmer_list,
+    #     "main_slider_image": main_slider_image,
+    # }
 
-    return render(request, "base/index.html", ctx)
+    return render(request, "base/landing/landing_page.html")
 
 
 def disclaimer(request):
