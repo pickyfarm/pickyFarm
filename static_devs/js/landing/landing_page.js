ae4ca214294aa8f559a20a3b72ede595c2424389
SJKim <hedro0458@gmail.com>
2021-09-24 16:44:35 +0900

    feat: fix navbar

--- static_devs/js/landing/landing_page.js
+++ static_devs/js/landing/landing_page.js
@@ -8,12 +8,6 @@ const storeButton = document.querySelector('a#header-list--store');
 const editorPickButton = document.querySelector('a#header-list--editors-pick');
 const farmerPageButton = document.querySelector('a#header-list--farmers-page');
 
-// Mobile navbar
-const navbarEditor = document.querySelector('#navbar-editor');
-const navbarFarmer = document.querySelector('#navbar-farmer');
-const navbarStore = document.querySelector('#navbar-store');
-const navbarMypage = document.querySelector('#navbar-mypage');
-
 function alertBasicAnnounce(e) {
     e.preventDefault();
     alert('10월 중순 오픈 예정입니다! 카톡 친추를 통해 알림을 받아보세요!');
@@ -33,9 +27,6 @@ myPageButton.addEventListener('click', function (e) {
     alertBasicAnnounce(e);
 });
 
-navbarMypage.addEventListener('click', function (e) {
-    alertBasicAnnounce(e);
-});
 // 장바구니
 cartButton.addEventListener('click', function (e) {
     alertBasicAnnounce(e);
@@ -46,57 +37,13 @@ editorPickButton.addEventListener('click', function (e) {
     alertBasicAnnounce(e);
 });
 
-navbarEditor.addEventListener('click', function (e) {
-    alertBasicAnnounce(e);
-});
-
 // Farmer's Page
 farmerPageButton.addEventListener('click', function (e) {
     alertBasicAnnounce(e);
 });
 
-navbarFarmer.addEventListener('click', function (e) {
-    alertBasicAnnounce(e);
-});
-
 // Store
 storeButton.addEventListener('click', function (e) {
-    const user_pk = storeButton.getAttribute('name');
-    if (user_pk !== 'None') {
-        e.preventDefault();
-        data = {
-            user_pk: user_pk,
-            csrfmiddlewaretoken: CSRF_TOKEN,
-        };
-        $.ajax({
-            type: 'POST',
-            url: landingTestURL,
-            dataType: 'json',
-            data: data,
-            success: function (response) {
-                if (response.status == 1) {
-                    window.location.href = storePageURL;
-                } else if (response.status == 0) {
-                    alert(
-                        '계정을 확인해주세요! (카드사 심사는 테스트 아이디로 로그인해주세요)'
-                    );
-                }
-            },
-            error: function (request, status, error) {
-                alert(
-                    '10월 중순 오픈 예정입니다! (카드사 심사는 테스트 아이디로 로그인해주세요)'
-                );
-            },
-        });
-    } else if (user_pk == 'None') {
-        e.preventDefault();
-        alert(
-            '10월 중순 오픈 예정입니다! (카드사 심사는 테스트 아이디로 로그인해주세요)'
-        );
-    }
-});
-
-navbarStore.addEventListener('click', function (e) {
     const user_pk = storeButton.getAttribute('name');
     if (user_pk !== 'None') {
         e.preventDefault();
