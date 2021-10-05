972a1f64afb02630f6499d13c05933efbdd3c7e2
SJKim <hedro0458@gmail.com>
2021-10-05 18:32:41 +0900

    feat: apply prohibit mobile access at farmer mypage

--- static_devs/js/base/prohibitMobile.js
+++ static_devs/js/base/prohibitMobile.js
@@ -2,15 +2,15 @@ const prohibitAccessFromMobile = (selectors) => {
     const mediaQuery = window.matchMedia('screen and (max-width: 768px)');
     const selectorArray = convertSelectorNameToNode(selectors);
 
+    const handleProhibitedElementClick = (e) => {
+        if (mediaQuery.matches) {
+            e.preventDefault();
+            alert('모바일에서 사용 불가능한 페이지입니다. PC로 접속해주세요.');
+        }
+    };
+
     selectorArray.forEach((selector) => {
-        selector.addeventListener('click', (e) => {
-            if (mediaQuery.matches()) {
-                e.preventDefault();
-                alert(
-                    '모바일에서 사용 불가능한 페이지입니다. PC로 접속해주세요.'
-                );
-            }
-        });
+        selector.addEventListener('click', handleProhibitedElementClick);
     });
 };
 
