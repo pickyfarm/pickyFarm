415f2114f2c920c93ec3d67fceab83f3edeba0b1
SJKim <hedro0458@gmail.com>
2021-10-06 17:35:35 +0900

    feat: set robots.txt

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 from . import views
 
 app_name = "core"
@@ -11,6 +12,10 @@ urlpatterns = [
     path("policy/disclaimer", views.disclaimer, name="disclaimer"),
     path("policy/personal-info", views.personal_info_usage, name="personal-info"),
     path("popup-callback", views.PopupCallback.as_view(), name="popup_callback"),
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
+    ),
 ]
 
 if settings.DEBUG:
