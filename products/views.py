2da7fbd50252c9d0565ff1f0da8114822ed5918c
InjiChoi <chldlswl1212@gmail.com>
2021-11-17 01:28:06 +0900

    fix: calculate product group total average in product detail view

--- products/views.py
+++ products/views.py
@@ -170,6 +170,7 @@ def product_detail(request, pk):
         # 연관 일반 작물
         related_product = product.related_product
 
+        product.product_group.calculate_total_rating_avg()
         # freshness
         if product.reviews != 0:
             freshness_per = product.product_group.calculate_freshness_rating_avg()
