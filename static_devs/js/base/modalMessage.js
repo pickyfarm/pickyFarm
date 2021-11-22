b37109184eef9ea19aa104cbb17a39aa51f6db60
SJKim <hedro0458@gmail.com>
2021-11-22 17:06:56 +0900

    feat: responsive modal close button

--- static_devs/js/base/modalMessage.js
+++ static_devs/js/base/modalMessage.js
@@ -1,26 +1,30 @@
 const showModalMessage = (message = '메세지를 입력하세요.', cb = null) => {
-  const modalSection = document.querySelector('#modal-section');
+    const modalSection = document.querySelector('#modal-section');
 
-  const displayModal = () => {
-    modalSection.style.display = 'flex';
-  };
+    const displayModal = () => {
+        modalSection.style.display = 'flex';
+    };
 
-  const hideModal = () => {
-    modalSection.style.display = 'none';
-  };
+    const hideModal = () => {
+        modalSection.style.display = 'none';
+    };
 
-  document.querySelector('#modal-message').innerHTML = message;
+    document.querySelector('#modal-message').innerHTML = message;
 
-  displayModal();
+    displayModal();
 
-  document
-    .querySelector('#modal-cancel')
-    .addEventListener('click', () => hideModal());
+    document
+        .querySelector('#modal-cancel')
+        .addEventListener('click', () => hideModal());
 
-  document.querySelector('#modal-accept').addEventListener('click', () => {
-    if (cb) {
-      cb();
-    }
-    hideModal();
-  });
+    document
+        .querySelector('#modal-close')
+        .addEventListener('click', () => hideModal());
+
+    document.querySelector('#modal-accept').addEventListener('click', () => {
+        if (cb) {
+            cb();
+        }
+        hideModal();
+    });
 };
