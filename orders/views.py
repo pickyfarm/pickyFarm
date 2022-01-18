2b9f49abf7323c086c30e4a019ee2267355c6f87
SJKim <hedro0458@gmail.com>
2022-01-18 21:51:57 +0900

    refactor: vbank_progress

--- orders/views.py
+++ orders/views.py
@@ -838,18 +838,47 @@ def vbank_progess(request):
 
     # 22.1.9 기윤 - 비회원/회원 분기 위한 추가
     cur_user = request.user
+    is_user = cur_user.is_authenticated
 
-    if cur_user.is_authenticated:
-        is_user = True
+    # [PROCESS 1] GET Parameter에 있는 pk 가져와서 Order_Group select / 구독 리스트 추출
+    order_group_pk = int(request.POST.get("order_group_pk"))
+    order_group = Order_Group.objects.get(pk=order_group_pk)
+    order_details = order_group.order_details.all()
+
+    # 22.1.19 기윤 - 비회원/회원 구분에 따른 추가
+    if is_user is True:
+        orderer_name = cur_user.account_name
+        orderer_phone_number = cur_user.phone_number
     else:
-        is_user = False
+        orderer_name = request.POST.get("orderer_name")
+        orderer_phone_number = request.POST.get("orderer_phone_number")
+
+    # values from client's form
+    rev_name = request.POST.get("rev_name")
+    rev_phone_number = request.POST.get("rev_phone_number")
+    rev_address = request.POST.get("rev_address")
+    rev_loc_at = request.POST.get("rev_loc_at")
+    rev_message = request.POST.get("rev_message")
+    to_farm_message = request.POST.get("to_farm_message")
+    payment_type = request.POST.get("payment_type")
+    order_group_name = request.POST.get("order_group_name")
+    client_total_price = int(request.POST.get("total_price"))
+
+    # 가상계좌 관련 정보
+    v_bank = request.POST.get("v_bank")
+    v_bank_account = request.POST.get("v_bank_account")
+    v_bank_account_holder = request.POST.get("v_bank_account_holder")
+    v_bank_expire_date_str = request.POST.get("v_bank_expire_date")
+    receipt_id = request.POST.get("receipt_id")
+    print(f"--------vbank : {v_bank} account : {v_bank_account}---------")
+
+    # 가상계좌 입금 마감 기한 datetime 변환
+    v_bank_expire_date = datetime.strptime(v_bank_expire_date_str, "%Y-%m-%d %H:%M:%S")
+    v_bank_expire_date = timezone.make_aware(v_bank_expire_date)
+    print(f"-----가상계좌 마감 기한 시간 변환 완료 : {v_bank_expire_date}---------")
 
     if request.method == "POST":
-        # [PROCESS 1] GET Parameter에 있는 pk 가져와서 Order_Group select / 구독 리스트 추출
-        order_group_pk = int(request.POST.get("order_group_pk"))
-        order_group = Order_Group.objects.get(pk=order_group_pk)
-        order_details = order_group.order_details.all()
-
+        # [Process #1-1] 구독 파머 리스트 추출
         farmers = list(set(map(lambda u: u.product.farmer, order_details)))
         unsubscribed_farmers = list()
         subscribed_farmers = list()
@@ -873,68 +902,51 @@ def vbank_progess(request):
         farmers_info = sorted(farmers_info, key=lambda x: x.farmer_pk)
         farmers_info_len = len(farmers_info)
 
-        # [PROCESS 2] 클라이언트에서 보낸 total_price와 서버의 total price 비교
-        client_total_price = int(request.POST.get("total_price"))
-
-        print(f"--------total_price : {client_total_price} ---------")
-
         try:
+            # [PROCESS 2] 클라이언트에서 보낸 total_price와 서버의 total price 비교
             if order_group.total_price != client_total_price:
                 raise priceMatchError
 
-        except priceMatchError:
-            print(priceMatchError)
-            order_group.status = "error_price_match"
-            for detail in order_group.order_details:
-                detail.status = "error_price_match"
-                detail.save()
-            order_group.save()
-            return redirect(
-                f'{reverse("orders:payment_fail")}?errorType=error_price_match&orderGroupPK={order_group_pk}'
-            )
+            # [PROCESS 3] Order_Group에 속한 Order_detail을 모두 가져와서 재고량 확인
+            # 모든 주문 상품 재고량 확인 태그
+            valid = True
 
-        # [PROCESS 3] Order_Group에 속한 Order_detail을 모두 가져와서 재고량 확인
+            # 재고가 부족한 상품명 리스트
+            invalid_products = list()
 
-        # 모든 주문 상품 재고량 확인 태그
-        valid = True
-        # 재고가 부족한 상품명 리스트
-        invalid_products = list()
+            # [PROCESS 4] 결제 전 최종 재고 확인
+            for detail in order_details:
+                print("[재고 확인 상품 재고] " + (str)(detail.product.stock))
+                print("[재고 확인 주문양] " + (str)(detail.quantity))
 
-        # [PROCESS 4] 결제 전 최종 재고 확인
-        for detail in order_details:
-            print("[재고 확인 상품 재고] " + (str)(detail.product.stock))
-            print("[재고 확인 주문양] " + (str)(detail.quantity))
-            if detail.product.stock - detail.quantity < 0:
-                valid = False
-                # 재고가 부족한 경우 부족한 상품 title 저장 -> 추후 결제 실패 페이지의 오류 메시지로 출력
-                invalid_products.append(detail.product.title)
-                print(detail.product.title + "재고 부족")
-            else:
-                # 재고가 있는 경우 재고 차감
-                detail.product.sold(detail.quantity)
-                detail.save()
+                if not detail.is_sufficient_stock():
+                    valid = False
 
-        print(invalid_products)
+                    # 재고가 부족한 경우 부족한 상품 title 저장 -> 추후 결제 실패 페이지의 오류 메시지로 출력
+                    invalid_products.append(detail.product.title)
 
-        try:
+                else:
+                    # 재고가 있는 경우 재고 차감
+                    detail.product.sold(detail.quantity)
+                    detail.save()
+
             # 재고가 없어서 valid가 False인경우 Exception 발생
             if valid is False:
                 print(f"--------재고 부족 ---------")
                 raise stockLackError
+
+        except priceMatchError:
+            order_group.set_order_state("error_price_match")
+
+            return redirect(
+                f'{reverse("orders:payment_fail")}?errorType=error_price_match&orderGroupPK={order_group_pk}'
+            )
+
         except stockLackError:
-            print(stockLackError)
-            order_group.status = "error_stock"
-            for detail in order_details:
-                detail.status = "error_stock"
-                detail.save()
-            order_group.save()
+            order_group.set_order_state("error_stock")
+
             print("[valid 값]" + (str)(valid))
             print("[invalid_products]" + (str)(invalid_products))
-            res_data = {
-                "valid": valid,
-                "error_type": "error_stock",
-                "invalid_products": invalid_products,
-            }
 
             return redirect(
                 f'{reverse("orders:payment_fail")}?errorType=error_stock&orderGroupPK={order_group_pk}&errorMsg={(str)(invalid_products)}의 재고가 부족합니다'
@@ -944,64 +956,27 @@ def vbank_progess(request):
         if valid is True:
             print(f"--------재고 확인 성공 ---------")
             # [PROCESS 6] 주문 정보 Order_Group에 등록
-
-            # 22.1.19 기윤 - 비회원/회원 구분에 따른 추가
-            if is_user is True:
-                orderer_name = cur_user.account_name
-                orderer_phone_number = cur_user.phone_number
-            else:
-                orderer_name = request.POST.get("orderer_name")
-                orderer_phone_number = request.POST.get("orderer_phone_number")
-
-            rev_name = request.POST.get("rev_name")
-            rev_phone_number = request.POST.get("rev_phone_number")
-            rev_address = request.POST.get("rev_address")
-            rev_loc_at = request.POST.get("rev_loc_at")
-            rev_message = request.POST.get("rev_message")
-            to_farm_message = request.POST.get("to_farm_message")
-            payment_type = request.POST.get("payment_type")
-            order_group_name = request.POST.get("order_group_name")
-
-            # 가상계좌 관련 정보
-            v_bank = request.POST.get("v_bank")
-            v_bank_account = request.POST.get("v_bank_account")
-            v_bank_account_holder = request.POST.get("v_bank_account_holder")
-            v_bank_expire_date_str = request.POST.get("v_bank_expire_date")
-            receipt_id = request.POST.get("receipt_id")
-            print(f"--------vbank : {v_bank} account : {v_bank_account}---------")
-            # 가상계좌 입금 마감 기한 datetime 변환
-            v_bank_expire_date = datetime.strptime(v_bank_expire_date_str, "%Y-%m-%d %H:%M:%S")
-            v_bank_expire_date = timezone.make_aware(v_bank_expire_date)
-            print(f"-----가상계좌 마감 기한 시간 변환 완료 : {v_bank_expire_date}---------")
-
-            print(rev_name + rev_phone_number + rev_loc_at + rev_message + to_farm_message)
-
-            print(order_group)
             # 배송 정보 order_group에 업데이트
-            # 22.1.9 기윤 - 비회원/회원 구분에 따른 orderer 정보 추가
-            order_group.orderer_name = orderer_name
-            order_group.orderer_phone_number = orderer_phone_number
-
-            order_group.rev_name = rev_name
-            order_group.rev_address = rev_address
-            order_group.rev_phone_number = rev_phone_number
-            order_group.rev_loc_at = rev_loc_at
-            # order_group.rev_loc_detail=rev_loc_detail
-            order_group.rev_message = rev_message
-            order_group.to_farm_message = to_farm_message
-            order_group.payment_type = payment_type
-
-            order_group.v_bank = v_bank
-            order_group.v_bank_account = v_bank_account
-            order_group.v_bank_account_holder = v_bank_account_holder
-            order_group.v_bank_expire_date = v_bank_expire_date
-            order_group.receipt_number = receipt_id
-
-            order_group.order_at = timezone.now()
-
-            order_group.status = "wait_vbank"
-
-            order_group.save()
+            order_group.update(
+                {
+                    "orderer_name": orderer_name,
+                    "orderer_phone_number": orderer_phone_number,
+                    "rev_name": rev_name,
+                    "rev_address": rev_address,
+                    "rev_phone_number": rev_phone_number,
+                    "rev_loc_at": rev_loc_at,
+                    "rev_message": rev_message,
+                    "to_farm_message": to_farm_message,
+                    "payment_type": payment_type,
+                    "v_bank": v_bank,
+                    "v_bank_account": v_bank_account,
+                    "v_bank_account_holder": v_bank_account_holder,
+                    "v_bank_expire_date": v_bank_expire_date,
+                    "receipt_number": receipt_id,
+                    "order_at": timezone.now(),
+                    "status": "wait_vbank",
+                }
+            )
 
             print(f"------order_group v_bank_expire_date {order_group.v_bank_expire_date}")
 
@@ -1016,7 +991,6 @@ def vbank_progess(request):
             }
 
             # 소비자 결제 완료 카카오 알림톡 전송
-            # 22.1.9 기윤 - order_group에 주문자 정보 추가로 인한 메시지 전송 타겟 수정
             send_kakao_message(
                 order_group.orderer_phone_number, templateIdList["vbank_info"], args_kakao
             )
@@ -1026,10 +1000,6 @@ def vbank_progess(request):
                 "order_details": order_details,
                 "sub_farmers": subscribed_farmers,
                 "unsub_farmers": unsubscribed_farmers,
-                # "v_bank" : v_bank,
-                # "v_bank_account" : v_bank_account,
-                # "v_bank_account_holder" : v_bank_account_holder,
-                # "v_bank_expire_date" : str(v_bank_expire_date),
             }
 
             nowDatetime = timezone.now().strftime("%Y-%m-%d %H:%M:%S")
