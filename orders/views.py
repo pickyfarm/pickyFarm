c34d0e1d6ffcdfbc33e245bc0863a57459c12844
SJKim <hedro0458@gmail.com>
2022-01-22 19:11:32 +0900

    fix: nonuser payment kakao message's link

--- orders/views.py
+++ orders/views.py
@@ -534,7 +534,7 @@ def send_kakao_with_payment_complete(order_group_pk, receipt_id):
             ###### 팝업 url 추가해야 ######
             args_consumer[
                 "#{link_2}"
-            ] = f"127.0.0.1:8000/user/mypage/orders/list?odmn={url_encoded_order_group_number}"  # 비회원용 구매확인 링크
+            ] = f"https://www.pickyfarm.com/user/mypage/orders/list?odmn={url_encoded_order_group_number}"  # 비회원용 구매확인 링크
 
         # 소비자 결제 완료 카카오 알림톡 전송
         send_kakao_message(
